65b79c30ff95696de6e628d2363c916c6e0f1a79
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-11-03 02:57:13 +0000

    error

--- stepper_control_back.py
+++ stepper_control_back.py
@@ -16,21 +16,24 @@ action=180
 MotorInput= Stepper(action, previous_angle,0x48)
 
 while True:
+  try:
+    with open('/usr/lib/cgi-bin/stepper-angle.txt','r') as f:
+      angleRead= json.load(f)
+      action= int(angleRead['NewAngle'])
+    
+    
+    if action== 0:
+      MotorInput.new_angle=0
+      print("motor zeroing")
+      MotorInput.zero()
+      GPIO.output(LED,0)
+      MotorInput.cur_angle= 0
+    else:
+      print("motor turning")
+      MotorInput.new_angle=action
+      MotorInput.goAngle()
+      MotorInput.cur_angle= action
+  except Exception as e:
+    print(e)
   
-  with open('/usr/lib/cgi-bin/stepper-angle.txt','r') as f:
-    angleRead= json.load(f)
-    action= int(angleRead['NewAngle'])
-  
-  
-  if action== 0:
-    MotorInput.new_angle=0
-    print("motor zeroing")
-    MotorInput.zero()
-    GPIO.output(LED,0)
-    MotorInput.cur_angle= 0
-  else:
-    print("motor turning")
-    MotorInput.new_angle=action
-    MotorInput.goAngle()
-    MotorInput.cur_angle= action
  
